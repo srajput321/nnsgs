3b2fbcdac684167d83253642be77f6e52b83d559
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-03-21 11:23:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1548904516").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Lover-Music/Lovermusic09")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Lover-Music/shubhamm")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
